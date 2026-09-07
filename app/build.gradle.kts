39cc8c943af3cbde28d614447f1c2bb914971d2b
saly90 <rnjbrnda4@gmail.com>
2026-09-07 22:03:34 +0330

    Add Android app module

---
+++ app/build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android { namespace = "com.xauai.gold"; compileSdk = 35
+    defaultConfig { applicationId = "com.xauai.gold"; minSdk = 26; targetSdk = 35; versionCode = 1; versionName = "1.0" }
+}
